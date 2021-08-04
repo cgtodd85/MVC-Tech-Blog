9892b246bb96d89f5e4de42109f7e734ec5d6375
Connor Todd <cgtodd85@gmail.com>
2021-08-03 21:40:18 -0400

    got main and homepage handlebars to work with server

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
-// const router = require('express').Router();
-// const userRoutes = require('./userRoutes');
-// const projectRoutes = require('./projectRoutes');
+const router = require("express").Router();
+// const userRoutes = require("./userRoutes");
+const homeRoutes = require("./homeRoutes");
 
-// router.use('/users', userRoutes);
-// router.use('/projects', projectRoutes);
+// router.use("/users", userRoutes);
+router.use("/", homeRoutes);
 
-// module.exports = router;
+module.exports = router;
