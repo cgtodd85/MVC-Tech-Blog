7a20ef733e696ee146d0f6191905c16f7dec66e9
Connor Todd <cgtodd85@gmail.com>
2021-08-05 15:39:56 -0400

    working homepage view using Post model

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,11 +4,26 @@ const { Post, User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
+    const allPosts = await Post.findAll();
+
+    // const allPosts = await Post.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       as: "post_author",
+    //       attributes: ["username"],
+    //     },
+    //   ],
+    // });
+    const posts = allPosts.map((post) => post.get({ plain: true }));
     // Pass serialized data and session flag into template
-    res.render("homepage");
+    console.log(posts);
+    res.render("homepage", { posts });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+/////////using below to test access to User model
 
 module.exports = router;
