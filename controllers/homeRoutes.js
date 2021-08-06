ff250524d489370e3f2b45c46e95541b391dab65
Connor Todd <cgtodd85@gmail.com>
2021-08-06 02:04:56 -0400

    working homepage with post and user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,17 +4,17 @@ const { Post, User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    const allPosts = await Post.findAll();
+    // const allPosts = await Post.findAll();
 
-    // const allPosts = await Post.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       as: "post_author",
-    //       attributes: ["username"],
-    //     },
-    //   ],
-    // });
+    const allPosts = await Post.findAll({
+      attributes: ["id", "user_id", "post_title", "blog_content"],
+      include: [
+        {
+          model: User,
+          as: "user",
+        },
+      ],
+    });
     const posts = allPosts.map((post) => post.get({ plain: true }));
     // Pass serialized data and session flag into template
     console.log(posts);
@@ -24,6 +24,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+// TODO test user model to see why getting eager loading error
 /////////using below to test access to User model
+
+router.get("/test", async (req, res) => {
+  try {
+    const allUsers = await User.findAll();
+    const users = allUsers.map((user) => user.get({ plain: true }));
+    console.log(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
