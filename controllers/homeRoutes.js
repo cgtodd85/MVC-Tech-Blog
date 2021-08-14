ebc85efe2ed31bd4869aca4084960575fdc0de6b
Connor Todd <cgtodd85@gmail.com>
2021-08-14 15:56:10 -0400

    single post page working with comments showing up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,40 +19,25 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/", async (req, res) => {
-//   try {
-//     // const allPosts = await Post.findAll();
-
-//     const allPosts = await Post.findAll({
-//       attributes: ["id", "user_id", "post_title", "blog_content"],
-//       include: [
-//         {
-//           model: User,
-//           as: "user",
-//         },
-//         // {
-//         //   model: Comment,
-//         //   as: "comment",
-//         // },
-//       ],
-//     });
-//     const posts = allPosts.map((post) => post.get({ plain: true }));
-//     // Pass serialized data and session flag into template
-//     console.log(posts);
-//     res.render("homepage", { posts });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// TODO test user model to see why getting eager loading error
-/////////using below to test access to User model
-
-router.get("/test", async (req, res) => {
+router.get("/post/:id", async (req, res) => {
   try {
-    const allUsers = await User.findAll();
-    const users = allUsers.map((user) => user.get({ plain: true }));
-    console.log(users);
+    const singlePost = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+        },
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+    const post = singlePost.get({ plain: true });
+    console.log(post);
+    res.render("single-post", { post });
   } catch (err) {
     res.status(500).json(err);
   }
