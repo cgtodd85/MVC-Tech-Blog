36a2b8ad73b532c8ac0dbb86ab2577c161a6c4b6
Connor Todd <cgtodd85@gmail.com>
2021-08-17 12:34:36 -0400

    changed name of css and added type back

--- config/connection.js
+++ config/connection.js
@@ -15,19 +15,5 @@ sequelize = process.env.JAWSDB_URL
         port: 3306,
       }
     );
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: "localhost",
-//       dialect: "mysql",
-//       port: 3306,
-//     }
-//   );
-// }
+    
 module.exports = sequelize;
