61ccc8d8481ba173835745426f2bc9d7be67b394
Connor Todd <cgtodd85@gmail.com>
2021-08-16 21:43:10 -0400

    server js update

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () =>
+  app.listen(port, () =>
     console.log("Now listening on port " + port + "!!!!!!!!!")
   );
 });
